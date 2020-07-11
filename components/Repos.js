570e62a95a6afe7936458cc10d709d7fae986f1f
Austin Tralnberg <atralnberg@gmail.com>
2020-07-10 20:48:18 -0600

    First Commit

---
+++ components/Repos.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Octicon, { Repo, Star, RepoForked, TriangleDown } from '@primer/octicons-react';
+import FlipMove from 'react-flip-move';
+import { langColors } from '../utils';
+import ReposStyles from './styles/ReposStyles';
+import DropdownStyles from './styles/DropdownStyles';
+import { Section } from '../style';
+
+
+
+const Repos = ({ repoData }) => {
+  const [topRepos, setTopRepos] = useState([]);
+  const [sortType, setSortType] = useState('likes');
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const getTopRepos = type => {
+    const LIMIT = 8;
+    const map = {
+      likes: 'likes_count',
+      retweets: 'retweets_count',
+      comments: 'comments_count',
+    };
+    const sortProperty = map[type];
+    const sorted = repoData
+      .filter(repo => !repo.fork)
+      .sort((a, b) => b[sortProperty] - a[sortProperty])
+      .slice(0, LIMIT);
+
+    setTopRepos(sorted);
+  };
+
+  useEffect(() => {
+    if (repoData.length) {
+      getTopRepos();
+    }
+  }, []);
+
+  useEffect(() => getTopRepos(sortType), [sortType]);
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+  const changeRepoSort = sortType => {
+    setSortType(sortType);
+    toggleDropdown();
+  };
+
+  const sortTypes = ['likes', 'retweets', 'comments'];
+
+  return (
+    <Section>
+      <ReposStyles>
+        <header>
+          <h2>Top Tweets</h2>
+          <div className="dropdown-wrapper">
+            <span className="label">by</span>
+            <DropdownStyles active={dropdownOpen}>
+              <button className="dropdown__button" onClick={() => toggleDropdown()}>
+                <label>{sortType}</label>
+                <Octicon icon={TriangleDown} />
+              </button>
+              <ul className="dropdown__list">
+                {sortTypes.map((type, i) => (
+                  <li className="dropdown__list-item" key={i}>
+                    <button onClick={() => changeRepoSort(type)}>{type}</button>
+                  </li>
+                ))}
+              </ul>
+            </DropdownStyles>
+          </div>
+        </header>
+
+        <div className="repo-list">
+          {topRepos.length > 0 ? (
+            <FlipMove typeName="ul">
+              {
+                topRepos.map(repo => (
+                <li key={repo.id}>
+                  {/* <blockquote class="twitter-tweet">
+                    <p lang="en" dir="ltr">{repo.body}</p>
+                    <p>Date: {repo.date}</p>
+                    <p>Likes: {repo.likes}</p>
+                    <p>Retweets: {repo.retweets}</p>
+                    <p>Hashtags: {repo.hashtags}</p>
+                  </blockquote> */}
+                  <a
+                    href={repo.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="repo">
+                    <div className="repo__top">
+                      <div className="repo__name">
+                        <Octicon icon={Repo} />
+                        <h3>{repo.date}</h3>
+                      </div>
+                      <p>{repo.body}</p>
+                    </div>
+                    <div className="repo__stats">
+                      <div className="repo__stats--left">
+                        <span>
+                          <div
+                            className="language"
+                            style={{ backgroundColor: "#89e051" }}
+                          />
+                          feedback
+                        </span>
+                        <span>
+                          <Octicon icon={Star} />
+                          {repo.likes}
+                        </span>
+                        <span>
+                          <Octicon icon={RepoForked} />
+                          {repo.retweets}
+                        </span>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              ))}
+            </FlipMove>
+          ) : (
+            <p>No available repositories!</p>
+          )}
+        </div>
+      </ReposStyles>
+    </Section>
+  );
+};
+
+Repos.propTypes = {
+  repoData: PropTypes.array.isRequired,
+};
+
+export default Repos;
---
+++ utils/mockMetaData.js
@@ -0,0 +1,5 @@
+export default [
+  { label: 'Positive', value: 24, color: '#89e051' },
+  { label: 'Neutral', value: 16, color: '#f1e05a' },
+  { label: 'Negative', value: 7, color: '#e34c26' },
+];
